99fe00bf5d591b7c018c1fc87fa6ab957808c641
ajha31 <ajha57775@gmail.com>
2020-06-07 21:18:30 +0530

    vehicle and city added

---
+++ index.js
@@ -0,0 +1,13 @@
+// var a=['no1','no2','no3','no4']
+//   var b=document.getElementById('label')
+//   var id = setInterval(frame, 2000);
+//   b.innerText=a[0]
+//   var i=1;
+//   function frame() {
+//     if (i == 4) {
+//       clearInterval(id);
+//     } else {
+//      b.innerText=a[i];
+//      i++; 
+//     } 
+//   }

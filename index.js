d6e4b1e038eeddce34e6a845133348d718ee913b
ajha31 <ajha57775@gmail.com>
2020-06-09 13:54:52 +0530

    final version with audio

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var a=[{date:"14/09/2018",place:"the nox",event:'rafaquat birthday' },
+var a=[ {date:"to  ",place:"be",event:'continued...'},
+        {date:"14/09/2018",place:"the nox",event:'rafaquat birthday' },
         {date:"24/08/2018",place:"hard rock cafe",event:'ayushi birthday'},
         {date:"18/05/2018",place:"college" ,event:'bobby birthday'},
         {date:"28/04/2018",place:"miraj" ,event:'infinity war'},
@@ -7,18 +8,21 @@ var a=[{date:"14/09/2018",place:"the nox",event:'rafaquat birthday' },
         {date:"19/12/2018",place:"eco park" ,event:'debo & poro party'},
         {date:"27/10/2019" ,place:"puri house",event:'diwali' },
         {date:"31/03/2019",place:"money sqare",event:'my birthday' },
-        {date:"27/11/2019",place:"raichuk",event:'long drive' },
-        {date:"to be continued...",place:"",event:''}
+        {date:"27/11/2019",place:"raichuk",event:'long drive' }
     ]
-  var b=document.getElementById('label')
-  var i=0;
-  b.innerHTML=`<ul><li>${a[i].date}</li><li>${a[i].place}</li><li>${a[i].event}</li><ul>`;
-  var id = setInterval(frame, 10000);
-  function frame() {
-    if (i == 11) {
-      clearInterval(id);
-    } else {
-      i++; 
+
+    setTimeout(() => {
+      var b=document.getElementById('label')
+      var i=10;
       b.innerHTML=`<ul><li>${a[i].date}</li><li>${a[i].place}</li><li>${a[i].event}</li><ul>`;
-    } 
-  }
+      var id = setInterval(frame, 10000);
+      function frame() {
+        if (i == 0) {
+          clearInterval(id);
+        } else {
+          i--; 
+          b.innerHTML=`<ul><li>${a[i].date}</li><li>${a[i].place}</li><li>${a[i].event}</li><ul>`;
+        } 
+      }
+    }, 4000);
+ 
